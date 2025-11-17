7a8109a919daa947f31bbfcb1072aef03573a462
AlanJhonatan <alan.amorim.franca@gmail.com>
2025-11-17 00:47:28 -0300

    feat: add ui event bus implementation

--- source/hooks/useSpotifyAuth.ts
+++ source/hooks/useSpotifyAuth.ts
@@ -5,13 +5,20 @@ export function useSpotifyAuth() {
 	const authFlow = AppFactory.createAuthServer();
 
 	const [authenticationUrl, setAuthenticationUrl] = useState<string>('');
+	const [authorizationCode, setAuthorizationCode] = useState<string>('');
+
 	useEffect(() => {
 		authFlow.setupServer();
 
 		setAuthenticationUrl(authFlow.getAuthenticationUrl());
+
+		AppFactory.eventBus.on('auth:spotify:success', (code: string) => {
+			setAuthorizationCode(code);
+		});
 	}, []);
 
 	return {
 		authenticationUrl,
+		authorizationCode,
 	};
 }
--- source/screens/auth.tsx
+++ source/screens/auth.tsx
@@ -5,11 +5,9 @@ import React from "react";
 import { useSpotifyAuth } from "../hooks/useSpotifyAuth.js";
 
 export function Auth() {
-	const { authenticationUrl } = useSpotifyAuth();
+	const { authenticationUrl, authorizationCode } = useSpotifyAuth();
 
 	useInput((input, key) => {
-		console.log(input, key.ctrl);
-
 		if(key.ctrl && input === 'c') {
 			process.exit(0);
 		}
@@ -20,18 +18,28 @@ export function Auth() {
             <Box marginBottom={1} flexDirection="column">
                 <Text underline bold>Spotify Authorization</Text>
             </Box>
-            <Box>
-                <Text><Spinner type="dots" /> Starting Authorization...</Text>
-            </Box>
-            <Box display="flex" flexDirection="column">
-                <Text>
-                    Access the spotify authorization page:
-                </Text>
-                <Newline />
-				<Link url={authenticationUrl}>
-                    <Text underline>{authenticationUrl}</Text>
-                </Link>
-            </Box>
+            {
+				authenticationUrl && !authorizationCode && (
+					<Box display="flex" flexDirection="column">
+						<Text>
+							<Spinner type="dots" /> Use the link and access the spotify's authorization page:
+						</Text>
+						<Newline />
+						<Link url={authenticationUrl}>
+							<Text underline>{authenticationUrl}</Text>
+						</Link>
+					</Box>
+				)
+			}
+			{
+				authenticationUrl && authorizationCode && (
+					<Box display="flex" flexDirection="column">
+						<Text>
+							✅ The authorization was successfull !
+						</Text>
+					</Box>
+				)
+			}
         </Box>
     )
 }
